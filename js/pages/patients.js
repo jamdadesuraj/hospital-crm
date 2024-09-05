2afaa3ccab7a2726dcb99f1fce5bfc9ab6acf5f3
unknown <surajjamdade83086@gmail.com>
2024-09-05 15:27:40 +0530

    first commit

---
+++ js/pages/patients.js
@@ -0,0 +1,17 @@
+$(function() {
+
+    'use strict';
+
+    $('#example1').DataTable({
+        'paging': true,
+        'lengthChange': false,
+        'searching': false,
+        'ordering': true,
+        'info': true,
+        'autoWidth': false
+    });
+
+
+
+
+}); // End of use strict
